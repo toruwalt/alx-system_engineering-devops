d7d01908371714a2a7e79fc7712c3ba4075de70a
Torubiri Walter <toruwalt1997@gmail.com>
2024-05-02 15:33:24 +0000

    Load Balancer - Task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,15 @@
+#A Bash Script that runs task 0 but as a Puppet file
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
