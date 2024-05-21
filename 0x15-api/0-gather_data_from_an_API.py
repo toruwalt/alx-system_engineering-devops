ee3e7ebf62372ab52b0b0f341c969d51787d6ec6
Torubiri Walter <toruwalt1997@gmail.com>
2024-05-21 00:27:41 +0000

    API - Task 0:Correction 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     nt = 0
     com_task = []
 
-    user = "https://jsonplaceholder.typicode.com/users/{}".format(name)
+    user = "https://jsonplaceholder.typicode.com/users/{}".format(name.json())
 
     response1 = requests.get(user)
     r1 = response1.json()
